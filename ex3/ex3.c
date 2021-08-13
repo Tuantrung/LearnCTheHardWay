83b08ec18064a69a257ffe8e7333fe1110dea978
Tuantrung <tuantrung03121997@gmail.com>
2021-08-14 01:11:37 +0700

    Completed to ex5

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+void crash(/* arguments */) {
+  char *test = NULL;
+  int i = 0;
 
+  for(i=0;i<1000000;i++){
+    printf("c",test[i]);
+  }
+}
 int main(void)
 {
   int age; //10
   int height = 72;
-  printf("I am %d years old.\n", age); //age
+  // printf(NULL); //age
   printf("I am %d inches tall.\n", height);
-
+  crash();
+  
   return 0;
 }
