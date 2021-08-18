145c437740feb358f49a32b0b261de55f775a81d
Tuantrung <tuantrung03121997@gmail.com>
2021-08-19 02:07:23 +0700

    Done exercise 10

---
+++ ex10/ex10_1.c
@@ -0,0 +1,20 @@
+/* this code covert the higher charater in the string to lower charater */
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  /* code */
+  if ( argc != 2) {
+    printf("ERROR: The argument must be one.\n");
+    // this is how you abort the program
+    return 1;
+  }
+  int i = 0;
+  for(i = 0; argv[1][i] != '\0'; i++) {
+    if ( argv[1][i] >= 'A'  && argv[1][i] < 'Z') {
+      argv[1][i] += 32;
+    } else continue ;
+  }
+  printf("%s.\n", argv[1]);
+  return 0;
+}
