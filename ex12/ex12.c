6e97de520f70ce01784cd6bb92de7ae1f5f2c000
Tuantrung <tuantrung03121997@gmail.com>
2021-10-04 18:14:19 +0700

    Done exercise 14

--- ex12/ex12.c
+++ ex12/ex12.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
   printf("The size of a char: %ld\n", sizeof(char));
   printf("The size of name (char[]): %ld\n", sizeof(name));
   printf("The number of chars: %ld\n\n", sizeof(name)/sizeof(char));
-
+  
   printf("The size of full_name (char[]): %ld\n", sizeof(full_name));
   printf("The number of chars: %ld\n", sizeof(full_name)/sizeof(char));
   printf("name=\"%s\" and full_name=\"%s\"\n\n", name, full_name);
