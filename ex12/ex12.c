bb5712b7b77fbc78f9061a9749c669cb29f4e09a
Tuantrung <tuantrung03121997@gmail.com>
2021-10-04 16:50:30 +0700

    Done exercise 13

--- ex12/ex12.c
+++ ex12/ex12.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
   printf("The size of an int: %ld\n", sizeof(int));
   printf("The size of areas (int[]): %ld\n", sizeof(areas));
   printf("The numbers of ints in areas: %ld\n", sizeof(areas) / sizeof(int));
-  printf("The first area is %d, the 2nd is %d.\n\n", areas[0], areas[1]);
+  printf("The first area is %c, the 2nd is %c.\n\n", (char)areas[0], (char)areas[1]);
 
   printf("The size of a char: %ld\n", sizeof(char));
   printf("The size of name (char[]): %ld\n", sizeof(name));
